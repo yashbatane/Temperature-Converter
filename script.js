89f09b1d6f797ddabdce199d88919d5ae822e4da
YASH BATANE <109904464+yashbatane@users.noreply.github.com>
2023-03-09 00:14:53 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const calculateTemp=()=>{
+    const numberTemp=document.getElementById('temp').value;
+    const tempSelected=document.getElementById('temp_diff');
+    const valueTemp=temp_diff.options[tempSelected.selectedIndex].value;
+
+    const celToFah=(cel)=>{
+        let fahrenheit = Math.round(cel * 9/5)+32;
+        return fahrenheit;
+    }
+
+    const FahToCel=(fah)=>{
+        let celcius = Math.round(fah -32)*5/9 ;
+        return celcius;
+    }
+
+    let result;
+    if(valueTemp=='cel'){
+        result=celToFah(numberTemp);
+        document.getElementById('resultContainer').innerHTML=`${result}°Fahrenheit`;
+    }else{
+        result=FahToCel(numberTemp);
+        document.getElementById('resultContainer').innerHTML=`${result} °Celcius`;}
+}
